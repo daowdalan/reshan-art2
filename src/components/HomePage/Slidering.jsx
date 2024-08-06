fae68442f02260e5abd7e53088087cae0aa8477a
alan <daowdalan@gmail.com>
2024-08-06 22:57:26 +0200

    make some changes

--- src/components/HomePage/Slidering.jsx
+++ src/components/HomePage/Slidering.jsx
@@ -13,11 +13,11 @@ function Slidering() {
         superLargeDesktop: {
             // the naming can be any, depends on you.
             breakpoint: { max: 4000, min: 3000 },
-            items: 5
+            items: 4    
         },
         desktop: {
             breakpoint: { max: 3000, min: 1024 },
-            items: 4
+            items: 3
         },
         tablet: {
             breakpoint: { max: 1024, min: 464 },
@@ -32,15 +32,19 @@ function Slidering() {
     return (
         <div className="my-10 px-2 ">
             <Carousel responsive={responsive}
+            
             >
                 {featuredArtworks.map((artwork, index) => (
                     <div key={index} className="flex justify-center w-full h-full p-1">
                         <Image
                         src={artwork.images[0].image}
                         alt="hero image"
-                        height={600}
-                        width={500}
-                        quality={100}
+                        width={550}
+                        height={550}
+                        style={{
+                            width: "auto",
+                            height: "100%"
+                        }}
                         />
                         
                     </div>
