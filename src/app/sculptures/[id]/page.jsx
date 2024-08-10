d28ca21a883ba68561f4abbba7e7e7a6052b45d3
daowdalan <85547033+daowdalan@users.noreply.github.com>
2024-08-10 22:16:10 +0200

    holy shit

--- src/app/sculptures/[id]/page.jsx
+++ src/app/sculptures/[id]/page.jsx
@@ -1,6 +1,7 @@
-"use client"
+"use client";
 import React from "react";
 import { useParams } from "next/navigation";
+import Image from "next/image";
 import artworksData from "../../data/artworksData";
 
 const SculpturePage = () => {
@@ -16,20 +17,36 @@ const SculpturePage = () => {
   }
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen m-20">
-      <h2 className="text-black text-4xl font-bold mb-4">{artwork.title}</h2>
-      <div className="flex flex-wrap justify-center mb-4">
+    <div className="flex flex-col md:flex-row items-start justify-between min-h-screen m-12 space-y-4 md:space-y-0 md:space-x-8">
+      {/* Left Side: Description */}
+      <div className="w-full md:w-1/2 md:sticky top-10 ">
+        <div className="mb-4">
+          <h2 className="text-black text-3xl font-bold">{artwork.title}</h2>
+          <p className="text-black text-lg">{artwork.description}</p>
+          {/* Add more artwork details as needed */}
+        </div>
+      </div>
+      
+      {/* Right Side: Images */}
+      <div className="w-full md:w-1/2 md:h-screen md:overflow-y-auto">
         {artwork.images.map((img, index) => (
-          <img
+          <div
             key={index}
-            src={img.image}
-            alt={`${artwork.title} image ${index + 1}`}
-            className="w-full h-auto max-w-md mb-4"
-          />
+            className="relative w-full mb-4 bg-gray-300"  // Apply grey background here
+          >
+            <Image
+              width={500}
+              height={400}
+              layout="responsive"
+              src={img.image}
+              alt={`${artwork.title} image ${index + 1}`}
+              className="transition-opacity duration-1000 ease-in-out opacity-0"
+              onLoad={(e) => e.target.classList.remove('opacity-0')}
+              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+            />
+          </div>
         ))}
       </div>
-      <p className="text-black text-lg">{artwork.description}</p>
-      {/* Add more artwork details as needed */}
     </div>
   );
 };
